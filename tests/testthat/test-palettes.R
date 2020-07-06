aa491e045cf7617cc3042830ec0217a77b6b5a82
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-07-06 09:38:04 +0100

    remove unnecessary tests

--- tests/testthat/test-palettes.R
+++ tests/testthat/test-palettes.R
@@ -4,7 +4,7 @@ test_that("incidence_pal", {
   skip_on_cran()
 
   expect_error(incidence_pal(NULL), "n is not a number")
-  for (n in 1:20) {
+  for (n in c(1, 5, 30)) {
     expect_length(incidence_pal(n), n)
   }
 
