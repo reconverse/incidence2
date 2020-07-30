71af8f0130ca1d4498a222076c8accfcf5f32f2b
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-07-30 09:56:25 +0100

    interval = 7 no longer treated like "week"

--- R/check_week.R
+++ R/check_week.R
@@ -6,8 +6,10 @@
 #'
 #' @noRd
 check_week <- function(the_interval) {
-  num_week <- is.numeric(the_interval) && the_interval == 7
-  int_week <- is.integer(the_interval) && the_interval == 7L
-  char_week <- is.character(the_interval) && grepl("week", the_interval, ignore.case = TRUE)
-  num_week || int_week || char_week
+  # TODO - check if we want interval = 7 treated like a week or not
+  # num_week <- is.numeric(the_interval) && the_interval == 7
+  # int_week <- is.integer(the_interval) && the_interval == 7L
+  # char_week <- is.character(the_interval) && grepl("week", the_interval, ignore.case = TRUE)
+  # num_week || int_week || char_week
+  is.character(the_interval) && grepl("week", the_interval, ignore.case = TRUE)
 }
