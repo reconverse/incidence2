0a3827e74bb6a0cd55e2cdbc970ebbdd7f5329d7
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-07-30 19:52:49 +0100

    added as_incidence function

--- R/conversion.R
+++ R/conversion.R
@@ -46,3 +46,59 @@ as_tibble.incidence <- function(x, ...) {
 #' @export
 #' @name as_tibble
 tibble::as_tibble()
+
+# -------------------------------------------------------------------------
+
+#' @rdname as_incidence
+#' @export
+as_incidence <- function(x, date_index, counts_var,
+                         group_vars = NULL, interval = 1L,
+                         standard = TRUE, ...) {
+
+  UseMethod("as_incidence", x)
+}
+
+
+#' @rdname as_incidence
+#' @aliases as_incidence.default
+#' @export
+as_incidence.default <- function(x, ...) {
+  stop(sprintf("Not implemented for class %s",
+               paste(class(x), collapse = ", ")))
+}
+
+
+#' @rdname as_incidence
+#' @aliases as_incidence.default
+#' @export
+as_incidence.data.frame <- function(x, date_index, counts_var,
+                                    group_vars = NULL, interval = 1L,
+                                    standard = TRUE, ...) {
+
+  # change date_index and group to character vectors
+  dates <- arg_values(!!rlang::enexpr(date_index))
+  count <- arg_values(!!rlang::enexpr(counts_var))
+  groups <- arg_values(!!rlang::enexpr(group_vars))
+
+  stopifnot(
+    "The argument `date_index` should be of length one" =
+      (length(dates) == 1),
+    "The argument `counts_var` should be of length one" =
+      (length(count) == 1),
+    "The argument `standard` must be either `TRUE` or `FALSE`." =
+      (is.logical(standard))
+  )
+
+  dat <- x[c(dates, groups)]
+  if (anyDuplicated(dat)) {
+    stop("Cannot convert a dataframe with duplicated rows into an incidence object")
+  }
+
+  dat <- dat[rep(seq_len(nrow(dat)), x[[count]]), ]
+  incidence(dat,
+            date_index = !!rlang::enexpr(dates),
+            groups = !!rlang::enexpr(groups),
+            interval = interval,
+            standard = standard)
+
+}
