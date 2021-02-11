866ac40c2e17c4d5634fd2531d007f4001815cba
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2021-02-11 22:24:54 +0000

    regroup uses data.table now

--- R/regroup.R
+++ R/regroup.R
@@ -36,20 +36,22 @@ regroup <- function(x, groups = NULL){
   groups <- rlang::enquo(groups)
   idx <- tidyselect::eval_select(groups, x)
   groups <- names(x)[idx]
-  column_names <- names(x)
-  check_presence(groups, column_names)
+  if (length(groups) == 0) {
+    groups <- NULL
+  } else {
+    column_names <- names(x)
+    check_presence(groups, column_names)
+  }
 
   date_var <- get_dates_name(x)
   count_var <- get_counts_name(x)
   cumulate <- attr(x, "cumulative")
   interval <- get_interval(x)
 
-  if (!is.null(groups)) {
-    tbl <- grouped_df(x, c(date_var, groups))
-  } else {
-    tbl <- grouped_df(x, date_var)
-  }
-  tbl <- summarise(tbl, count = sum(.data[[count_var]]))
+  tbl <- as.data.table(x)
+  tbl <- tbl[,.(count = sum(get(..count_var))), by = c(date_var, groups)]
+  setDF(tbl)
+
 
   # create subclass of tibble
   tbl <- tibble::new_tibble(tbl,
