9f472b1c63f7bdc929ab63c67689408f9fc5d2e3
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2021-02-17 15:42:45 +0000

    better print formatting

--- R/print.R
+++ R/print.R
@@ -18,24 +18,24 @@ print.incidence2 <- function(x, ...) {
 
   # cases over date range
   cat(sprintf(
-    "[%d cases from days %s to %s]\n",
+    "%d cases from days %s to %s\n",
     sum(x[[count_var]]), min(x[[date_var]]), max(x[[date_var]])
   ))
 
   # interval
   interval <- get_interval(x)
   if (is.integer(interval)) {
-    cat(sprintf("[interval: %d %s]\n", interval, ifelse(interval < 2, "day", "days")))
+    cat(sprintf("interval: %d %s\n", interval, ifelse(interval < 2, "day", "days")))
   } else if (grepl("\\d", interval)) {
-    cat(sprintf("[interval: %s]\n", interval))
+    cat(sprintf("interval: %s\n", interval))
   } else {
-    cat(sprintf("[interval: 1 %s]\n", interval))
+    cat(sprintf("interval: 1 %s\n", interval))
   }
 
   # cumulative
   cumulative <- attr(x, "cumulative")
   if (!is.null(cumulative)) {
-    cat(sprintf("[cumulative: %s]\n\n", cumulative))
+    cat(sprintf("cumulative: %s\n\n", cumulative))
   }
 
   # print tibble but remove title
