6cc295bab82fa5b216f4f875dd4f6ce5c5d77811
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-01-13 17:12:41 +0000

    wip - dplyr integration - cumulate

--- R/incidence2-package.R
+++ R/incidence2-package.R
@@ -5,7 +5,7 @@
 ## usethis namespace: end
 NULL
 
-#' @importFrom dplyr across all_of bind_rows count grouped_df rename summarise
+#' @importFrom dplyr across all_of bind_rows count grouped_df mutate rename summarise ungroup
 #' @importFrom grDevices colorRampPalette
 #' @importFrom pillar tbl_sum
 #' @importFrom tidyr pivot_longer
