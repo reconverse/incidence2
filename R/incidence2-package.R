ac92c5ef809152803715c34487467b112b21ac27
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-01-17 09:27:39 +0000

    provide a non data.table check for duplicate rows
    Added a use of vctrs::vec_duplicate_any within the subsetting code for when
    list columns are present.
    
    We could only use the vctrs::vec_duplicate_any function but I want to
    explicitly branch on non-data.table use as it will allow us to easily
    drop dplyr support should we ever wish to do so.

--- R/incidence2-package.R
+++ R/incidence2-package.R
@@ -5,9 +5,10 @@
 ## usethis namespace: end
 NULL
 
+#' @import data.table
 #' @importFrom dplyr across all_of bind_rows count grouped_df mutate rename summarise ungroup
 #' @importFrom grDevices colorRampPalette
 #' @importFrom pillar tbl_sum
 #' @importFrom tidyr pivot_longer
-#' @import data.table
+#' @importFrom vctrs vec_duplicate_any
 NULL
