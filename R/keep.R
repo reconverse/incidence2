5749ef4ecbfec9ffeadec8bd2d7a3746fdb73f95
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-01-20 17:21:58 +0000

    keep funs now optionally calls complete_dates (TRUE by default)

--- R/keep.R
+++ R/keep.R
@@ -1,7 +1,7 @@
 #' Keep first and last occurences
 #'
-#' `keep_first()` (`keep_last`) keeps the first (last) `n` entries to occur by
-#' date ordering.
+#' `keep_first()` (`keep_last`) keeps the first (last) `n` rows to occur for
+#' each group when in ascending date order.
 #'
 #' @param x `[incidence]` object.
 #'
@@ -10,6 +10,17 @@
 #' Number of entries to keep.
 #'
 #' `double` vectors will be converted via `as.integer(n)`.
+#'
+#' @param complete_dates `[bool]`
+#'
+#' Should `complete_dates()` be called on the data prior to keeping the first
+#' entries.
+#'
+#' Defaults to TRUE.
+#'
+#' @param ...
+#'
+#' Other arguments passed to `complete_dates()`.
 #'
 #' @return
 #'
@@ -31,27 +42,7 @@ NULL
 
 #' @rdname keep
 #' @export
-keep_first <- function(x, n) {
-
-    if (!inherits(x, "incidence"))
-        stopf("`x` must be an incidence object.")
-
-    if (!is.integer(n)) {
-        if (is.vector(n, "double")) {
-            n <- as.integer(n)
-        } else {
-            stopf("`n` must be integer.")
-        }
-    }
-
-    dat <- get_dates(x)[[1L]]
-    idx <- keep_idx(dat, n = n, from_last = FALSE)
-    x[idx, ]
-}
-
-#' @rdname keep
-#' @export
-keep_last <- function(x, n) {
+keep_first <- function(x, n, complete_dates = TRUE, ...) {
 
     if (!inherits(x, "incidence"))
         stopf("`x` must be an incidence object.")
@@ -64,10 +55,40 @@ keep_last <- function(x, n) {
         }
     }
     if (n <= 0L)
-        stop("`n` must be non-negative")
+        stopf("`n` must be non-negative")
+
+    .assert_bool(complete_dates)
+    if (complete_dates)
+        x <- complete_dates(x, ...)
 
     dat <- get_dates(x)[[1L]]
-    idx <- keep_idx(dat, n = n, from_last = TRUE)
+    idx <- .keep_idx(dat, n = n, from_last = FALSE)
+    x[idx, ]
+}
+
+#' @rdname keep
+#' @export
+keep_last <- function(x, n, complete_dates = TRUE, ...) {
+
+    if (!inherits(x, "incidence"))
+        stopf("`x` must be an incidence object.")
+
+    if (!is.integer(n)) {
+        if (is.vector(n, "double")) {
+            n <- as.integer(n)
+        } else {
+            stopf("`n` must be integer.")
+        }
+    }
+    if (n <= 0L)
+        stopf("`n` must be non-negative")
+
+    .assert_bool(complete_dates)
+    if (complete_dates)
+        x <- complete_dates(x, ...)
+
+    dat <- get_dates(x)[[1L]]
+    idx <- .keep_idx(dat, n = n, from_last = TRUE)
     x[idx, ]
 }
 
@@ -77,7 +98,7 @@ keep_last <- function(x, n) {
 # ------------------------------------------------------------------------- #
 # ------------------------------------------------------------------------- #
 
-keep_idx <- function(x, n, from_last = FALSE) {
+.keep_idx <- function(x, n, from_last = FALSE) {
     uniq <- sort(unique(x), decreasing = FALSE)
     len <- length(uniq)
     if (n > len) {
