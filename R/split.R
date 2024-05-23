97e22bd5aa349a9e3679fa20a8960bfc61e9fb37
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-05-23 13:44:31 +0100

    docs: add help page for split.incidence2()

--- R/split.R
+++ R/split.R
@@ -1,3 +1,36 @@
+#' Divide an incidence2 object in to it's implicit groupings
+#'
+#' Split divides and [incidence2][incidence2::incidence] object in to it's
+#' underlying groupings (count variable and optionally groups).
+#'
+#' @param x An [incidence2][incidence2::incidence] object.
+#'
+#' @param f Not used. Present only for generic compatibility.
+#'
+#' @param drop Not used. Present only for generic compatibility.
+#'
+#' @param ... Not used. Present only for generic compatibility.
+#'
+#' @return
+#'
+#' A list of tibbles contained the split data. This list also has a "key"
+#' attribute which is a tibble with rows corresponding to the grouping of
+#' each split.
+#'
+#' @examples
+#' \dontshow{.old <- data.table::setDTthreads(2)}
+#' if (requireNamespace("outbreaks", quietly = TRUE)) {
+#'     data(ebola_sim_clean, package = "outbreaks")
+#'     ebola_sim_clean$linelist |>
+#'         dplyr::filter(!is.na(hospital)) |>
+#'         incidence_(date_of_onset, hospital, interval = "isoweek") |>
+#'         split()
+#' }
+#' \dontshow{data.table::setDTthreads(.old)}
+#'
+#' @seealso
+#' [vctrs::vec_split()] on which `split.incidence2()` is built.
+#'
 #' @export
 split.incidence2 <- function(x, f, drop, ...) {
 
@@ -10,5 +43,9 @@ split.incidence2 <- function(x, f, drop, ...) {
 
     # note split.data.table seems to fail so we use vctrs::vec_split
     x <- tibble::as_tibble(x)
-    vctrs::vec_split(x, x[groupings])[["val"]]
+    split <- vctrs::vec_split(x, x[groupings])
+    key <- split[["key"]]
+    split <- split[["val"]]
+    attr(split, "key") <- key
+    split
 }
