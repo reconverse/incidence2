2624daac8253dbfce577056ec93611f4f28c2440
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-05-30 12:58:37 +0100

    feat: turn off custom errors for time being

--- R/error_types.R
+++ R/error_types.R
@@ -1,3 +1,8 @@
 error_types <- new.env()
-error_types$argument <- "incidence2_argument_error"
-error_types$suggested <- "incidence2_suggested_error"
+
+# TODO - think about turning this on
+#default <- function(x) c(x, "incidence2_error")
+#error_types$argument <- default("incidence2_argument_error")
+#error_types$suggested <- default("incidence2_suggested_error")
+
+error_types$argument <- error_types$suggested <- NULL
