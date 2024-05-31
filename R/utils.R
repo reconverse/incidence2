19b073f403d55f27acbd22039f7370d11eb1f48a
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-05-31 09:39:34 +0100

    refactor: remove unused function

--- R/utils.R
+++ R/utils.R
@@ -11,13 +11,6 @@
     stop(errorCondition(msg, class = error_types$suggested, call = .call[1L]))
 }
 
-stopf <- function(fmt, ..., .use_call = TRUE, .call = sys.call(-1L)) {
-    .call <- if (isTRUE(.use_call)) .call[1L] else NULL
-    msg <- sprintf(fmt, ...)
-    err <- simpleError(msg, .call)
-    stop(err)
-}
-
 warnf <- function(fmt, ..., .use_call = TRUE, .call = sys.call(-1L)) {
     .call <- if (isTRUE(.use_call)) .call[1L] else NULL
     msg <- sprintf(fmt, ...)
