67fe53677c7f20167ab46b2d4b9e3317f58183a0
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-08-30 18:17:30 +0100

    tests: add as_incidence() tests

---
+++ tests/testthat/_snaps/as_incidence.md
@@ -0,0 +1,8 @@
+# Default dispatch gives error
+
+    Code
+      as_incidence(mtcars)
+    Condition
+      Error in `as_incidence.default()`:
+      ! Not implemented for objects of class <data.frame>.
+
---
+++ tests/testthat/test-as_incidence.R
@@ -0,0 +1,10 @@
+test_that("Default dispatch gives error", {
+    expect_snapshot(error = TRUE, as_incidence(mtcars))
+})
+
+test_that("as_incidence works as expected", {
+    skip_if_not_installed("outbreaks")
+    dat <- outbreaks::ebola_sim_clean$linelist
+    i <- incidence(dat, date_index = "date_of_onset", groups = c("gender", "hospital"))
+    expect_identical(as_incidence(i), i)
+})
