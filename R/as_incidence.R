07688cccd9d81eebb15b091bedc305062a2ae0ef
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-08-30 09:08:42 +0100

    lint: finally decided to use toString() after initial reluctance

--- R/as_incidence.R
+++ R/as_incidence.R
@@ -20,7 +20,7 @@ as_incidence <- function(x, ...) {
 #' @rdname as_incidence
 #' @export
 as_incidence.default <- function(x, ...) {
-    .stopf_argument("Not implemented for class [%s].", paste(class(x), collapse = ", "))
+    .stopf_argument("Not implemented for class [%s].", toString(class(x)))
 }
 
 #' @rdname as_incidence
